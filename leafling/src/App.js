167f1a2104b219309e143cf1da5263b5ad12a4ac
anshi1108 <anshitiwari1108@gmail.com>
2024-09-14 13:52:45 +0530

    added learners guide

--- leafling/src/App.js
+++ leafling/src/App.js
@@ -5,7 +5,9 @@ import Signup from './Components/Signup/Signup'
 import Login from './Components/Login/Login'
 import Home from './Components/Home/Home'
 import Marketplace from './Components/Marketplace/Marketplace';
-import Info from './Components/info/Info'
+import Info from './Components/info/Info';
+import Guide from './Components/Learner_guide/Guide_home';
+import Cactus from './Components/Learner_guide/Cactus';
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
       <Route path='/home' element={<Home />} />
       <Route path='/marketplace' element={<Marketplace />} />
       <Route path='/info' element={<Info />} />
+      <Route path='/guide' element={<Guide />} />
+      <Route path='/cactus' element={<Cactus />} />
     </Routes>
   );
 }
