4022f6637652b422057b8dbbbf993ec3d43ce87f
Anish1005 <143640454+Anish1005@users.noreply.github.com>
2024-09-20 21:36:05 +0530

    Update Forumcard.js

--- leafling/src/Components/Home/Forumcard.js
+++ leafling/src/Components/Home/Forumcard.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import './Forumcard.css';
+import { useNavigate } from 'react-router-dom';
+import './Forumcard.css'; 
 
-const ForumCard = ({ name, image, description }) => {
+const Forumcard = ({ name, image, description, route }) => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate(route);
+    };
+
     return (
-        <div className="forum-card">
-            <img src={image} alt={name} className="forum-image" />
-            <div className="forum-details">
-                <h3>{name}</h3>
-                <p>{description}</p>
+        <div className='forum-card' onClick={handleClick}>
+            <img src={image} alt={name} className='forum-image' />
+            <div className='forum-info'>
+                <h3 className='forum-name'>{name}</h3>
+                <p className='forum-description'>{description}</p>
             </div>
         </div>
     );
 };
 
-export default ForumCard;
+export default Forumcard;
